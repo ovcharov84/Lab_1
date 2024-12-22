0cbd87ad0e57f7d1f1e509ddbd6f4c7d640b3f3a
Калашнікова Вікторія <v.o.kalashnikova@student.khai.edu>
2024-12-22 13:39:41 +0200

    Add interfaces

---
+++ Lab_1/IAdd.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab_1
+{
+    public interface IAdd
+    {
+        double Add(double a, double b);
+    }
+}
---
+++ Lab_1/IMultiply.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab_1
+{
+    public interface IMultiply
+    {
+        double Multiply(double a, double b);
+    }
+}
