c1475323464c6deb4250284d37f30c2bbeeb6863
Овчаров Антон Володимирович <a.v.ovcharov@student.khai.edu>
2024-12-23 19:29:43 +0200

    Add API endpoints

--- CalculatorApi/Program.cs
+++ CalculatorApi/Program.cs
@@ -2,6 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<Add>();
+builder.Services.AddSingleton<Subtract>();
+builder.Services.AddSingleton<Multiply>();
+builder.Services.AddSingleton<Divide>();
 
 var app = builder.Build();
 
@@ -13,5 +17,28 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapGet("/add", (double a, double b, [FromServices] Add calculator) =>
+{
+    return calculator.Add(a, b);
+})
+.WithOpenApi();
+
+app.MapGet("/subtract", (double a, double b, [FromServices] Subtract calculator) =>
+{
+    return calculator.Subtract(a, b);
+})
+.WithOpenApi();
+
+app.MapGet("/divide", (double a, double b, [FromServices] Divide calculator) =>
+{
+    return calculator.Divide(a, b);
+})
+.WithOpenApi();
+
+app.MapGet("/multiply", (double a, double b, [FromServices] Multiply calculator) =>
+{
+    return calculator.Multiply(a, b);
+})
+.WithOpenApi();
 
 app.Run();
